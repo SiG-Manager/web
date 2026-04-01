edbe15d2997fd484d121044f45b738a704c4ad5b
sudzekai <yomakum@mail.ru>
2026-04-01 09:44:41 +0300

    фикс бага с отображением на телефоне

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import Const from "../assets/Const"
 
 function NotFound() {
     return (
-        <div className=" min-vw-100 d-flex flex-column align-items-center gap-1">
+        <div className="d-flex flex-column align-items-center gap-1">
             <h1 className=" display-1 text-center mt-5">404</h1>
             <h4 className=" display-5 text-center">Страница не найдена</h4>
             <Link className="btn btn-primary mt-3"
